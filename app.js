88e3b7a3bae5c52d18158066384fec98187cf90d
mizan <mizzanur007@gmail.com>
2020-09-29 12:46:24 +0600

    ✔ refactored the callback, promise land with async await function

--- app.js
+++ app.js
@@ -9,36 +9,26 @@ const wait = (ms = 0) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const box = document.querySelector(".box");
 
-function animate(e) {
+async function animate(e) {
   // change the text to GO when clicked
   const el = e.currentTarget;
   el.textContent = "GO";
 
-  wait(200)
-    .then(() => {
-      // Make it a circle after 2secs
-      el.classList.add("circle");
-      return wait(500);
-    })
-    .then(() => {
-      // Make it red after 0.5s
-      el.classList.add("red");
-      return wait(500);
-    })
-    .then(() => {
-      // Make it square after 0.25s
-      el.classList.remove("circle");
-      return wait(500);
-    })
-    .then(() => {
-      // Make it purple after 0.3s
-      el.classList.add("purple");
-      return wait(500);
-    })
-    .then(() => {
-      // fade away the box
-      el.classList.add("fade");
-    });
+  await wait(200);
+  // Make it a circle after 2secs
+  el.classList.add("circle");
+  await wait(500);
+  // Make it red after 0.5s
+  el.classList.add("red");
+  await wait(500);
+  // Make it square after 0.25s
+  el.classList.remove("circle");
+  await wait(500);
+  // Make it purple after 0.3s
+  el.classList.add("purple");
+  await wait(500);
+  // fade away the box
+  el.classList.add("fade");
 }
 
 box.addEventListener("click", animate);
